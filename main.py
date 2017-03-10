ae1b26a850317f565b070db28feec599c0fc49ff
David Buckley <dbuckl02@mail.uoguelph.ca>
2017-03-10 00:16:01 -0500

    fixing updated argument parsing

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def _encrypt(args):
             f.seek(0)
             RC5.encrypt_file(f, out, key, args.blocksize, args.rounds)
     else:    
-        with open('{}'.format(args.text), 'rb') as f, open(args.outfile, 'wb') as out:
+        with open('{}'.format(args.infile), 'rb') as f, open(args.outfile, 'wb') as out:
             RC5.encrypt_file(f, out, key, args.blocksize, args.rounds)
 
     stored_key = Key(key, args.blocksize, args.keysize, args.rounds)
@@ -38,7 +38,7 @@ def _decrypt(args):
             f.seek(0)
             RC5.decrypt_file(f, out, key.key, key.blocksize, key.rounds)
     else:
-        with open('{}'.format(args.text), 'rb') as f, open(args.outfile, 'wb') as out:
+        with open('{}'.format(args.infile), 'rb') as f, open(args.outfile, 'wb') as out:
             RC5.decrypt_file(f, out, key.key, key.blocksize, key.rounds)
 
 
@@ -57,29 +57,29 @@ def _rounds_type(x):
 if __name__ == '__main__':
     #Main argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('text', help="The plaintext wanted to be encrypted/decrypted")
     parser.add_argument('--cmdtext', action='store_true',
-                        help="Specify this argument if the text entered on the commandline is the text to \
-operate on, it is by default treated as a filename")
+                        help="Specifiy this command to treat the 'infile' argument as the text the encrypt")
 
-    subparsers = parser.add_subparsers(dest='operation', help='Option to specify to encrypt or decrypt the input')
+    subparsers = parser.add_subparsers(dest='operation')
     subparsers.required = True
     
     #Subparser for encryption
     parser_e = subparsers.add_parser('encrypt', help='Option to select to encrypt input')
+    parser_e.add_argument('infile', help="The plaintext wanted to be encrypted/decrypted")
     parser_e.add_argument('--blocksize', type=int, choices=(32, 64, 128), default=64,
-                        help="The block size in bits for the cipher to operate on the input data (32, 64, 128)")
+                        help="The block size in bits for the cipher to operate on the input data (32, 64, 128), default 64")
     
     parser_e.add_argument('--keysize', type=_keysize_type, default=128,
-                        help="The key size in bits for the cihper to generate (0-2040)")
+                        help="The key size in bits for the cihper to generate (0-2040), default 128")
 
     parser_e.add_argument('--rounds', type=_rounds_type, default=12,
-                        help="The number of rounds used in the key expansion (0-255)")
+                        help="The number of rounds used in the key expansion (0-255), default 12")
     parser_e.add_argument('outfile', help="Name of output encrypted file")
     parser_e.set_defaults(func=_encrypt)
 
     #Subparer for decryption
     parser_d = subparsers.add_parser('decrypt', help='Option to select to decrypt input')
+    parser_d.add_argument('infile', help="The plaintext wanted to be encrypted/decrypted")
     parser_d.add_argument('key', help="The file generated holding the key during encryption")
     parser_d.add_argument('outfile', help="Name of output unencrypted file")
     parser_d.set_defaults(func=_decrypt)
