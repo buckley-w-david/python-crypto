67140c5dc4e449c13fb73785de8ad1e40d234d29
David Buckley <buckley.w.david@gmail.com>
2018-07-20 03:31:34 -0400

    fix formatting, and run dos2unix on everything

--- python_crypto/cli.py
+++ python_crypto/cli.py
@@ -6,6 +6,7 @@ from io import BytesIO
 import pickle
 from python_crypto.symmetric import rc5
 
+
 class Key:
     def __init__(self, key, blocksize, keysize, rounds):
         self.key = key
@@ -13,81 +14,114 @@ class Key:
         self.keysize = keysize
         self.rounds = rounds
 
+
 def _encrypt(args):
-    key = randbits(args.keysize).to_bytes(args.keysize//8, byteorder='little')
+    key = randbits(args.keysize).to_bytes(args.keysize // 8, byteorder="little")
     cipher = rc5.RC5(key, args.blocksize, args.rounds)
 
-    if (args.cmdtext):
-        with open(args.outfile, 'wb') as out:
+    if args.cmdtext:
+        with open(args.outfile, "wb") as out:
             out.write(cipher.encrypt(args.text.encode()))
     else:
-        with open('{}'.format(args.infile), 'rb') as f, open(args.outfile, 'wb') as out:
+        with open("{}".format(args.infile), "rb") as f, open(args.outfile, "wb") as out:
             out.write(cipher.encrypt(f.read()))
 
     stored_key = Key(key, args.blocksize, args.keysize, args.rounds)
-    with open('{}.key'.format(args.outfile), 'wb') as out:
-        pickle.dump(stored_key, open("{}.key".format(args.outfile), 'wb'))
+    with open("{}.key".format(args.outfile), "wb") as out:
+        pickle.dump(stored_key, open("{}.key".format(args.outfile), "wb"))
+
 
 def _decrypt(args):
     key = None
-    with open(args.key, 'rb') as f:
+    with open(args.key, "rb") as f:
         key = pickle.load(f)
     cipher = rc5.RC5(key.key, key.blocksize, key.rounds)
 
     if args.cmdtext:
-        with BytesIO() as f, open(args.outfile, 'wb') as out:
+        with BytesIO() as f, open(args.outfile, "wb") as out:
             out.write(cipher.decrypt(args.text.encode()))
     else:
-        with open('{}'.format(args.infile), 'rb') as f, open(args.outfile, 'wb') as out:
+        with open("{}".format(args.infile), "rb") as f, open(args.outfile, "wb") as out:
             out.write(cipher.decrypt(f.read()))
 
 
 def _keysize_type(x):
     x = int(x)
     if x < 0 or x > 2040:
-        raise argparse.ArgumentTypeError("invalid choice: {} (choose from 0-2040)".format(x))
+        raise argparse.ArgumentTypeError(
+            "invalid choice: {} (choose from 0-2040)".format(x)
+        )
     return x
+
 
 def _rounds_type(x):
     x = int(x)
     if x < 0 or x > 255:
-        raise argparse.ArgumentTypeError("invalid choice: {} (choose from 0-255)".format(x))
+        raise argparse.ArgumentTypeError(
+            "invalid choice: {} (choose from 0-255)".format(x)
+        )
     return x
 
 
 def main():
-    #Main argument parser
+    # Main argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cmdtext', action='store_true',
-                        help="Specifiy this command to treat the 'infile' argument as the text the encrypt")
+    parser.add_argument(
+        "--cmdtext",
+        action="store_true",
+        help="Specifiy this command to treat the 'infile' argument as the text the encrypt",
+    )
 
-    subparsers = parser.add_subparsers(dest='operation')
+    subparsers = parser.add_subparsers(dest="operation")
     subparsers.required = True
 
-    #Subparser for encryption
-    parser_e = subparsers.add_parser('encrypt', help='Option to select to encrypt input')
-    parser_e.add_argument('infile', help="The plaintext wanted to be encrypted/decrypted")
-    parser_e.add_argument('--blocksize', type=int, choices=(32, 64, 128), default=64,
-                        help="The block size in bits for the cipher to operate on the input data (32, 64, 128), default 64")
+    # Subparser for encryption
+    parser_e = subparsers.add_parser(
+        "encrypt", help="Option to select to encrypt input"
+    )
+    parser_e.add_argument(
+        "infile", help="The plaintext wanted to be encrypted/decrypted"
+    )
+    parser_e.add_argument(
+        "--blocksize",
+        type=int,
+        choices=(32, 64, 128),
+        default=64,
+        help="The block size in bits for the cipher to operate on the input data (32, 64, 128), default 64",
+    )
 
-    parser_e.add_argument('--keysize', type=_keysize_type, default=128,
-                        help="The key size in bits for the cihper to generate (0-2040), default 128")
+    parser_e.add_argument(
+        "--keysize",
+        type=_keysize_type,
+        default=128,
+        help="The key size in bits for the cihper to generate (0-2040), default 128",
+    )
 
-    parser_e.add_argument('--rounds', type=_rounds_type, default=12,
-                        help="The number of rounds used in the key expansion (0-255), default 12")
-    parser_e.add_argument('outfile', help="Name of output encrypted file")
+    parser_e.add_argument(
+        "--rounds",
+        type=_rounds_type,
+        default=12,
+        help="The number of rounds used in the key expansion (0-255), default 12",
+    )
+    parser_e.add_argument("outfile", help="Name of output encrypted file")
     parser_e.set_defaults(func=_encrypt)
 
-    #Subparer for decryption
-    parser_d = subparsers.add_parser('decrypt', help='Option to select to decrypt input')
-    parser_d.add_argument('infile', help="The plaintext wanted to be encrypted/decrypted")
-    parser_d.add_argument('key', help="The file generated holding the key during encryption")
-    parser_d.add_argument('outfile', help="Name of output unencrypted file")
+    # Subparer for decryption
+    parser_d = subparsers.add_parser(
+        "decrypt", help="Option to select to decrypt input"
+    )
+    parser_d.add_argument(
+        "infile", help="The plaintext wanted to be encrypted/decrypted"
+    )
+    parser_d.add_argument(
+        "key", help="The file generated holding the key during encryption"
+    )
+    parser_d.add_argument("outfile", help="Name of output unencrypted file")
     parser_d.set_defaults(func=_decrypt)
 
     args = parser.parse_args()
     args.func(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- python_crypto/symmetric/rc5.py
+++ python_crypto/symmetric/rc5.py
@@ -1,4 +1,4 @@
-'''Please don't ever write Python like this, it's terrible'''
+"""Please don't ever write Python like this, it's terrible"""
 import math
 import typing
 
@@ -6,108 +6,123 @@ from python_crypto.cipher import Cipher
 
 
 def _rotate_left(val: int, r_bits: int, max_bits: int) -> int:
-    v1 = (val << r_bits%max_bits) & (2**max_bits-1)
-    v2 = ((val & (2**max_bits-1)) >> (max_bits-(r_bits%max_bits)))
+    v1 = (val << r_bits % max_bits) & (2 ** max_bits - 1)
+    v2 = (val & (2 ** max_bits - 1)) >> (max_bits - (r_bits % max_bits))
     return v1 | v2
+
 
 def _rotate_right(val: int, r_bits: int, max_bits: int) -> int:
-    v1 = ((val & (2**max_bits-1)) >> r_bits%max_bits)
-    v2 = (val << (max_bits-(r_bits%max_bits)) & (2**max_bits-1))
+    v1 = (val & (2 ** max_bits - 1)) >> r_bits % max_bits
+    v2 = val << (max_bits - (r_bits % max_bits)) & (2 ** max_bits - 1)
 
     return v1 | v2
 
+
 def _expand_key(key: bytes, wordsize: int, rounds: int) -> typing.List[int]:
-    #Pads key so that it is aligned with the word size, then splits it into words
+    # Pads key so that it is aligned with the word size, then splits it into words
     def _align_key(key: bytes, align_val: int) -> typing.List[int]:
         while len(key) % (align_val):
-            key += b'\x00' #Add 0 bytes until the key length is aligned to the block size
+            key += (
+                b"\x00"
+            )  # Add 0 bytes until the key length is aligned to the block size
 
         L = []
         for i in range(0, len(key), align_val):
-            L.append(int.from_bytes(key[i:i+align_val], byteorder='little'))
+            L.append(int.from_bytes(key[i : i + align_val], byteorder="little"))
 
         return L
 
-    #generation function of the constants for the extend step
+    # generation function of the constants for the extend step
     def _const(w: int) -> typing.Tuple[int, int]:
         if w == 16:
-            return (0xB7E1, 0x9E37) # Returns the value of P and Q
+            return (0xB7E1, 0x9E37)  # Returns the value of P and Q
         elif w == 32:
             return (0xB7E15163, 0x9E3779B9)
         elif w == 64:
             return (0xB7E151628AED2A6B, 0x9E3779B97F4A7C15)
-        raise ValueError('Bad word sie')
+        raise ValueError("Bad word sie")
 
-    #Generate pseudo-random list S
+    # Generate pseudo-random list S
     def _extend_key(w: int, r: int) -> typing.List[int]:
         P, Q = _const(w)
         S = [P]
-        t = 2*(r+1)
+        t = 2 * (r + 1)
         for i in range(1, t):
-            S.append((S[i-1]+Q) % 2**w)
+            S.append((S[i - 1] + Q) % 2 ** w)
 
         return S
 
-    def _mix(L: typing.List[int], S: typing.List[int], r: int, w: int, c: int) -> typing.List[int]:
-        t = 2*(r+1)
+    def _mix(
+        L: typing.List[int], S: typing.List[int], r: int, w: int, c: int
+    ) -> typing.List[int]:
+        t = 2 * (r + 1)
         m = max(c, t)
         A = B = i = j = 0
 
-        for k in range(3*m):
-            A = S[i] = _rotate_left(S[i]+A+B, 3, w)
-            B = L[j] = _rotate_left(L[j]+A+B, A+B, w)
+        for k in range(3 * m):
+            A = S[i] = _rotate_left(S[i] + A + B, 3, w)
+            B = L[j] = _rotate_left(L[j] + A + B, A + B, w)
 
-            i = (i+1) % t
-            j = (j+1) % c
+            i = (i + 1) % t
+            j = (j + 1) % c
 
         return S
 
-    aligned = _align_key(key, wordsize//8)
+    aligned = _align_key(key, wordsize // 8)
     extended = _extend_key(wordsize, rounds)
 
     S = _mix(aligned, extended, rounds, wordsize, len(aligned))
 
     return S
 
-def _encrypt_block(data: bytes, expanded_key: typing.List[int], blocksize: int, rounds: int) -> bytes:
-    w = blocksize//2
-    b = blocksize//8
-    mod = 2**w
 
-    A = int.from_bytes(data[:b//2], byteorder='little')
-    B = int.from_bytes(data[b//2:], byteorder='little')
+def _encrypt_block(
+    data: bytes, expanded_key: typing.List[int], blocksize: int, rounds: int
+) -> bytes:
+    w = blocksize // 2
+    b = blocksize // 8
+    mod = 2 ** w
 
-    A = (A+expanded_key[0]) % mod
-    B = (B+expanded_key[1]) % mod
+    A = int.from_bytes(data[: b // 2], byteorder="little")
+    B = int.from_bytes(data[b // 2 :], byteorder="little")
 
-    for i in range(1, rounds+1):
-        A = (_rotate_left((A^B), B, w) + expanded_key[2 * i]) % mod
-        B = (_rotate_left((A^B), A, w) + expanded_key[2 * i + 1]) % mod
+    A = (A + expanded_key[0]) % mod
+    B = (B + expanded_key[1]) % mod
 
-    res = A.to_bytes(b//2, byteorder='little') + B.to_bytes(b//2, byteorder='little')
+    for i in range(1, rounds + 1):
+        A = (_rotate_left((A ^ B), B, w) + expanded_key[2 * i]) % mod
+        B = (_rotate_left((A ^ B), A, w) + expanded_key[2 * i + 1]) % mod
+
+    res = A.to_bytes(b // 2, byteorder="little") + B.to_bytes(
+        b // 2, byteorder="little"
+    )
     return res
 
-def _decrypt_block(data: bytes, expanded_key: typing.List[int], blocksize: int, rounds: int) -> bytes:
-    w = blocksize//2
-    b = blocksize//8
-    mod = 2**w
 
-    A = int.from_bytes(data[:b//2], byteorder='little')
-    B = int.from_bytes(data[b//2:], byteorder='little')
+def _decrypt_block(
+    data: bytes, expanded_key: typing.List[int], blocksize: int, rounds: int
+) -> bytes:
+    w = blocksize // 2
+    b = blocksize // 8
+    mod = 2 ** w
+
+    A = int.from_bytes(data[: b // 2], byteorder="little")
+    B = int.from_bytes(data[b // 2 :], byteorder="little")
 
     for i in range(rounds, 0, -1):
-        B = _rotate_right(B-expanded_key[2*i+1], A, w)^A
-        A = _rotate_right((A-expanded_key[2*i]), B, w)^B
+        B = _rotate_right(B - expanded_key[2 * i + 1], A, w) ^ A
+        A = _rotate_right((A - expanded_key[2 * i]), B, w) ^ B
 
     B = (B - expanded_key[1]) % mod
     A = (A - expanded_key[0]) % mod
 
-    res = A.to_bytes(b//2, byteorder='little') + B.to_bytes(b//2, byteorder='little')
+    res = A.to_bytes(b // 2, byteorder="little") + B.to_bytes(
+        b // 2, byteorder="little"
+    )
     return res
 
 
 class RC5(Cipher):
-
     def __init__(self, key: bytes, blocksize: int, rounds: int) -> None:
         self.key = key
         self.blocksize = blocksize
@@ -118,8 +133,8 @@ class RC5(Cipher):
         key = self.key
         rounds = self.rounds
 
-        w = blocksize//2
-        b = blocksize//8
+        w = blocksize // 2
+        b = blocksize // 8
 
         expanded_key = _expand_key(key, w, rounds)
 
@@ -127,21 +142,21 @@ class RC5(Cipher):
         chunk = data[:index]
         out = []
         while chunk:
-            chunk = chunk.ljust(b, b'\x00') #padding with 0 bytes if not large enough
+            chunk = chunk.ljust(b, b"\x00")  # padding with 0 bytes if not large enough
             encrypted_chunk = _encrypt_block(chunk, expanded_key, blocksize, rounds)
             out.append(encrypted_chunk)
 
-            chunk = data[index:index+b] #Read in blocksize number of bytes
-            index+=b
-        return b''.join(out)
+            chunk = data[index : index + b]  # Read in blocksize number of bytes
+            index += b
+        return b"".join(out)
 
     def decrypt(self, data: bytes) -> bytes:
         blocksize = self.blocksize
         key = self.key
         rounds = self.rounds
 
-        w = blocksize//2
-        b = blocksize//8
+        w = blocksize // 2
+        b = blocksize // 8
 
         expanded_key = _expand_key(key, w, rounds)
 
@@ -150,10 +165,10 @@ class RC5(Cipher):
         out = []
         while chunk:
             decrypted_chunk = _decrypt_block(chunk, expanded_key, blocksize, rounds)
-            chunk = data[index:index+b] #Read in blocksize number of bytes
+            chunk = data[index : index + b]  # Read in blocksize number of bytes
             if not chunk:
-                decrypted_chunk = decrypted_chunk.rstrip(b'\x00')
+                decrypted_chunk = decrypted_chunk.rstrip(b"\x00")
 
-            index+=b
+            index += b
             out.append(decrypted_chunk)
-        return b''.join(out)
+        return b"".join(out)
